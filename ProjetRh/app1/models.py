309d30cc57567a818fd38eff0aff1e1394102480
BENSTITI MAROUA DALILA <136383561+maroubns@users.noreply.github.com>
2024-12-23 15:35:03 +0100

    Update models.py

--- ProjetRh/app1/models.py
+++ ProjetRh/app1/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Employe(models.Model):
+    nom= models.CharField(max_length=20)
+    prenom= models.CharField(max_length=20)
+    date_de_naissance= models.DateField(default=timezone.now)
+    date_aumbauche= models.DateField(default=timezone.now)
+    adresse= models.CharField(max_length=30)
+    Historique_professionnel= models.CharField(max_length=500)
+    competance= models.CharField(max_length= 500)
+    formation= models.CharField(max_length=500)
+
+
+class Salaire
