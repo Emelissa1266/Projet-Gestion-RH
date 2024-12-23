7bc78ccc6b5245e5ac3e74b2f457231e4c9d6c2e
BENSTITI MAROUA DALILA <136383561+maroubns@users.noreply.github.com>
2024-12-23 16:52:29 +0100

    m

--- ProjetRh/app1/admin.py
+++ ProjetRh/app1/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Employe
+from .models import Evaluation
+from .models import Conge
+from .models import Contrat
+from .models import Salaire
+from .models import Service
 
 # Register your models here.
+admin.site.register(Employe)
+admin.site.register(Evaluation)
+admin.site.register(Conge)
+admin.site.register(Contrat)
+admin.site.register(Salaire)
+admin.site.register(Service)
+
--- ProjetRh/app1/models.py
+++ ProjetRh/app1/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Employe(models.Model):
@@ -8,8 +9,50 @@ class Employe(models.Model):
     date_aumbauche= models.DateField(default=timezone.now)
     adresse= models.CharField(max_length=30)
     Historique_professionnel= models.CharField(max_length=500)
-    competance= models.CharField(max_length= 500)
-    formation= models.CharField(max_length=500)
+    Service_Employe= models.ForeignKey(Service, on_delete=models.CASCADE)
+
+    
+
+class Salaire(models.Model):
+    mois= models.DateField(default=timezone.now)
+    annee= models.DateField(default=timezone.now)
+    salaire_base= models.FloatField(max_length=15)
+    primes=models.FloatField(max_length=15)
+    heures_supplementaires= models.IntegerField(max_length=5)
+    retenus= models.FloatField(max_length=15)
+    salaire_net= models.FloatField(max_length=20)
+    Employe_salaire= models.ForeignKey(Employe, on_delete=models.CASCADE)
 
 
-class Salaire
+class Evaluation(models.Model):
+    date_Evaluation= models.DateField(default=timezone.now)
+    criteres= models.CharField(max_length=200)
+    Resultat= models.CharField(max_length=200)
+    Commentaire= models.CharField(max_length=500)
+    Employe_Evaluation= models.ForeignKey(Employe, on_delete=models.CASCADE)
+
+
+class Service(models.Model):
+    Nom= models.CharField(max_length=20)
+    description= models.CharField(max_length=200)
+
+
+
+class Conge(models.Model):
+    date_deb= models.DateField(default=timezone.now)
+    date_fin= models.DateField(default=timezone.now)
+    type_conge= models.CharField(max_length=15)
+    solde_conge= models.FloatField(max_length= 10)
+    Employe_Conge= models.ForeignKey(Employe, on_delete=models.CASCADE)
+
+
+
+class Contrat(models.Model):
+     date_deb= models.DateField(default=timezone.now)
+     date_fin= models.DateField(default=timezone.now)
+     type_contrat= models.CharField(max_length=15)
+     salaire_monsuel= models.FloatField(max_length=20)
+     salaire_quotidien= models.FloatField(max_length=20)
+     Employe_Contrat= models.ForeignKey(Employe, on_delete=models.CASCADE)
+
+     
