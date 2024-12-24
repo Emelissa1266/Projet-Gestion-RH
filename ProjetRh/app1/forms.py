6580e7e10e704dc89ae8b93b1dba6ecf7072a648
MELISSA JOUDA <150617628+Emelissa1266@users.noreply.github.com>
2024-12-24 22:57:28 +0100

    Homecondida
    ceation de la template home

--- ProjetRh/app1/forms.py
+++ ProjetRh/app1/forms.py
@@ -1,5 +1,18 @@
 from django import forms
+from .models import Utilisateur
 
 class AuthentificationForm(forms.Form):
     login = forms.CharField(label="Login", max_length=30, widget=forms.TextInput(attrs={"class": "form-control"}))
     mot_de_passe = forms.CharField(label="Mot de passe", widget=forms.PasswordInput(attrs={"class": "form-control"}))
+
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = Utilisateur
+        fields = ['Login', 'mot_de_passe']
+        widgets = {
+            'mot_de_passe': forms.PasswordInput(attrs={'placeholder': 'Mot de passe'}),
+        }
+        labels = {
+            'Login': 'Nom d\'utilisateur',
+            'mot_de_passe': 'Mot de passe',
+        }
--- ProjetRh/app1/views.py
+++ ProjetRh/app1/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from .forms import AuthentificationForm
 from .models import Utilisateur
+from .forms import SignupForm
+
+def home_candidat(request):
+    return render(request, 'home_candidat.html')
+
 
 def authentifier(request):
     if request.method == "POST":
@@ -17,4 +22,17 @@ def authentifier(request):
                 form.add_error(None, "Login ou mot de passe incorrect !")
     else:
         form = AuthentificationForm()
-    return render(request, "authentification.html", {"form": form})
+    return render(request, "login.html", {"form": form})
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            utilisateur = form.save(commit=False)
+            utilisateur.role = 'candidate'  # Par défaut, le rôle est "candidate"
+            utilisateur.save()
+            return redirect('connexion')  # Redirige vers la page de connexion
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
